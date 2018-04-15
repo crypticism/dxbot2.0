48084b9cd48a8966734dc55f3f686bbe947ef354
tomatosoupcan <benjamin.chuba@gmail.com>
2018-04-15 12:00:32 -0400

    add lookup function and improve quotes to crash less

--- dxbot2.py
+++ dxbot2.py
@@ -107,7 +107,11 @@ def handle_command(command, args, channel, prev):
             response = addQuote(args, users)
         else:
             response = getQuote(args, users)
-
+    if command.startswith('lookup'):
+        if args is not None and len(args.split()) = 1:
+            response = getQuoteByLookup(args, users)
+        else:
+            response = 'Too many arguments provided'
     if command.startswith('grab'):
         message = '{} {}'.format(user_map[prev['user']], prev['text'])
         response = addQuote(message, users)
