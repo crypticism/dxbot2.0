d47f15af4369fd1eac0e161502165e0243f9f7d6
Joe Kari <kari7003@gmail.com>
2018-04-14 23:43:50 -0400

    Refactor get quotes functions.

--- lib/quotes.py
+++ lib/quotes.py
@@ -15,6 +15,75 @@ def addQuote(args, users):
     return 'Not implemented yet.'
 
 
+def getRandomQuote():
+    """
+    Retrieve a random quote from the database.
+    """
+    conn = psycopg2.connect("dbname=dxbot user=postgres host=localhost")
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM quotes;')
+    (count,) = cur.fetchone()
+    if count:
+        index = random.randint(1, count)
+        cur.execute('SELECT * FROM quotes WHERE id = {};'.format(index))
+        (num, name, quote) = cur.fetchone()
+        cur.close()
+        return '#{}: {} - "{}"'.format(num, name, quote)
+    cur.close()
+    return 'There are no quotes.'
+
+
+def getQuoteByID(args):
+    """
+    Retrieve a quote by a specific ID.
+    """
+    conn = psycopg2.connect("dbname=dxbot user=postgres host=localhost")
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM quotes;')
+    (count,) = cur.fetchone()
+    if int(args) > count:
+        cur.close()
+        return 'There aren\'t that many quotes.'
+
+    cur.execute('SELECT * FROM quotes WHERE id = {};'.format(args))
+    (_, name, quote) = cur.fetchone()
+    cur.close()
+    return '{} - "{}"'.format(name, quote)
+
+
+def getQuoteCount():
+    """
+    Retrieve the number of quotes in the database.
+    """
+    conn = psycopg2.connect("dbname=dxbot user=postgres host=localhost")
+    cur = conn.cursor()
+    cur.execute('SELECT COUNT(*) FROM quotes;')
+    (count,) = cur.fetchone()
+    cur.close()
+    return 'There are {} quotes.'.format(count)
+
+
+def getQuoteByName(args, users):
+    """
+    Retrieve a random quote from a specific user.
+    """
+    if args.strip() in users:
+        conn = psycopg2.connect("dbname=dxbot user=postgres host=localhost")
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM quotes WHERE name = \'{}\';'.format(args))
+        quotes = cur.fetchall()
+        if not len(quotes):
+            cur.close()
+            return '{} has no quotes.'.format(args)
+
+        index = random.randint(0, len(quotes) - 1)
+        (num, name, quote) = quotes[index]
+        cur.close()
+        return '#{}: {} - "{}"'.format(num, name, quote)
+    else:
+        return '{} is not a valid user.'.format(args)
+
+
 def getQuote(args, users):
     """
     Retrieves quotes from the database based on the type of arg passed in
@@ -23,53 +92,16 @@ def getQuote(args, users):
     # - retrieves the count of quotes
     string - retrieves a random quote with a username matching that string
     """
-    conn = psycopg2.connect("dbname=dxbot user=postgres host=localhost")
-    cur = conn.cursor()
     if args is None:
-        cur.execute('SELECT COUNT(*) FROM quotes;')
-        (count,) = cur.fetchone()
-        if count:
-            index = random.randint(1, count)
-            cur.execute('SELECT * FROM quotes WHERE id = {};'.format(index))
-            (num, name, quote) = cur.fetchone()
-            cur.close()
-            return '#{}: {} - "{}"'.format(num, name, quote)
-        cur.close()
-        return 'There are no quotes.'
+        return getRandomQuote()
 
     if isInt(args):
-        cur.execute('SELECT COUNT(*) FROM quotes;')
-        (count,) = cur.fetchone()
-        if int(args) > count:
-            cur.close()
-            return 'There aren\'t that many quotes.'
-
-        cur.execute('SELECT * FROM quotes WHERE id = {};'.format(args))
-        (_, name, quote) = cur.fetchone()
-        cur.close()
-        return '{} - "{}"'.format(name, quote)
+        return getQuoteByID(args)
 
     if args == '#':
-        cur.execute('SELECT COUNT(*) FROM quotes;')
-        (count,) = cur.fetchone()
-        cur.close()
-        return 'There are {} quotes.'.format(count)
+        return getQuoteCount()
 
     if type(args) is str:
-        if args.strip() in users:
-            cur.execute('SELECT * FROM quotes WHERE name = \'{}\';'.format(args))
-            quotes = cur.fetchall()
-            if not len(quotes):
-                cur.close()
-                return '{} has no quotes.'.format(args)
+        return getQuoteByName(args, users)
 
-            index = random.randint(0, len(quotes) - 1)
-            (num, name, quote) = quotes[index]
-            cur.close()
-            return '#{}: {} - "{}"'.format(num, name, quote)
-        else:
-            cur.close()
-            return '{} is not a valid user.'.format(args)
-
-    cur.close()
     return 'Not implemented yet.'
