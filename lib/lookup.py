bc3d2adc9f2f89fd90770d3c408a1ae41f60173b
tomatosoupcan <benjamin.chuba@gmail.com>
2018-08-18 12:04:09 -0400

    add elookup

--- lib/lookup.py
+++ lib/lookup.py
@@ -64,11 +64,9 @@ def getLookupCount(args, users):
     searchString = args.replace('#', '').strip()
 
     sql = """
-        SELECT *
+        SELECT COUNT(*)
         FROM quotes
         WHERE quote ILIKE '%%%s%%'
-        ORDER BY RANDOM()
-        LIMIT 1;
     """ % args
 
     cur.execute(sql)
@@ -79,3 +77,70 @@ def getLookupCount(args, users):
         return "There are {} quotes containing {}.".format(count, searchString)
 
     return "There are no quotes with {} in it.".format(searchString)
+
+def getQuoteByLookupExact(args, users):
+    """
+    Retrieve a random quote containing a specific word exactly.
+    """
+    updateUsageCount('Lookup Quote Exact')
+
+    if not re.search('^[\w\s]+$', args):
+        return "That's not a safe string. Please try again."
+
+    conn = psycopg2.connect(CONNECT_STRING)
+    cur = conn.cursor()
+
+    sql = """
+        SELECT COUNT(*) FROM quotes WHERE quote ILIKE '%%%s%%'
+    """ % args
+
+    cur.execute(sql)
+    (count,) = cur.fetchone()
+
+    if not count:
+        cur.close()
+        return 'No quotes with exactly {} in it'.format(args)
+
+    sql = """
+        SELECT *
+        FROM quotes
+        WHERE quote quote ~* '.*\y%s\y.*'
+        ORDER BY RANDOM()
+        LIMIT 1;
+    """ % args
+
+    cur.execute(sql)
+    (num, name, quote) = cur.fetchone()
+    cur.close()
+
+    return '#{}: {} - "{}"'.format(num, name, quote)
+
+
+def getLookupCountExact(args, users):
+    """
+    Retrieve the count of quotes with a specific word exactly
+    """
+    updateUsageCount('Lookup Count Exact')
+
+    conn = psycopg2.connect(CONNECT_STRING)
+    cur = conn.cursor()
+
+    if not re.search('^[\w\s]+#$', args):
+        return "That's not a safe string. Please try again."
+
+    searchString = args.replace('#', '').strip()
+
+    sql = """
+        SELECT COUNT(*)
+        FROM quotes
+        WHERE quote ~* '.*\y%s\y.*'
+    """ % args
+
+    cur.execute(sql)
+    (count,) = cur.fetchone()
+    cur.close()
+
+    if count:
+        return "There are {} quotes containing exactly {}.".format(count, searchString)
+
+    return "There are no quotes with exactly {} in it.".format(searchString)
